33c3fe1c1083e73b85c54f09dff49e84dbbb4789
Mumin Koykiran <muminkoykiran@gmail.com>
2020-02-04 23:19:04 +0000

    Orange Pi adapter and conditions for defining this device have been created.

--- Adafruit_GPIO/GPIO.py
+++ Adafruit_GPIO/GPIO.py
@@ -253,6 +253,108 @@ class RPiGPIOAdapter(BaseGPIO):
         else:
             self.rpi_gpio.cleanup(pin)
 
+class OPiGPIOAdapter(BaseGPIO):
+    """GPIO implementation for the Raspberry Pi using the OPi.GPIO library."""
+
+    def __init__(self, opi_gpio, mode=None):
+        self.opi_gpio = opi_gpio
+        # Suppress warnings about GPIO in use.
+        opi_gpio.setwarnings(False)
+        
+        # Setup board
+        opi_gpio.setboard(opi_gpio.PCPCPLUS)
+        
+        # Setup board pin mode.
+        if mode == opi_gpio.BOARD or mode == opi_gpio.BCM:
+            opi_gpio.setmode(mode)
+        elif mode is not None:
+            raise ValueError('Unexpected value for mode.  Must be BOARD or BCM.')
+        else:
+            # Default to BCM numbering if not told otherwise.
+            opi_gpio.setmode(opi_gpio.BCM)
+        # Define mapping of Adafruit GPIO library constants to OPi.GPIO constants.
+        self._dir_mapping = { OUT:      opi_gpio.OUT,
+                              IN:       opi_gpio.IN }
+        self._pud_mapping = { PUD_OFF:  opi_gpio.PUD_OFF,
+                              PUD_DOWN: opi_gpio.PUD_DOWN,
+                              PUD_UP:   opi_gpio.PUD_UP }
+        self._edge_mapping = { RISING:  opi_gpio.RISING,
+                               FALLING: opi_gpio.FALLING,
+                               BOTH:    opi_gpio.BOTH }
+
+    def setup(self, pin, mode, pull_up_down=PUD_OFF):
+        """Set the input or output mode for a specified pin.  Mode should be
+        either OUTPUT or INPUT.
+        """
+        self.opi_gpio.setup(pin, self._dir_mapping[mode],
+                            pull_up_down=self._pud_mapping[pull_up_down])
+
+    def output(self, pin, value):
+        """Set the specified pin the provided high/low value.  Value should be
+        either HIGH/LOW or a boolean (true = high).
+        """
+        self.opi_gpio.output(pin, value)
+
+    def input(self, pin):
+        """Read the specified pin and return HIGH/true if the pin is pulled high,
+        or LOW/false if pulled low.
+        """
+        return self.opi_gpio.input(pin)
+
+    def input_pins(self, pins):
+        """Read multiple pins specified in the given list and return list of pin values
+        GPIO.HIGH/True if the pin is pulled high, or GPIO.LOW/False if pulled low.
+        """
+        # maybe opi has a mass read...  it would be more efficient to use it if it exists
+        return [self.opi_gpio.input(pin) for pin in pins]
+
+    def add_event_detect(self, pin, edge, callback=None, bouncetime=-1):
+        """Enable edge detection events for a particular GPIO channel.  Pin 
+        should be type IN.  Edge must be RISING, FALLING or BOTH.  Callback is a
+        function for the event.  Bouncetime is switch bounce timeout in ms for
+        callback
+        """
+        kwargs = {}
+        if callback:
+            kwargs['callback']=callback
+        if bouncetime > 0:
+            kwargs['bouncetime']=bouncetime
+        self.opi_gpio.add_event_detect(pin, self._edge_mapping[edge], **kwargs)
+
+    def remove_event_detect(self, pin):
+        """Remove edge detection for a particular GPIO channel.  Pin should be
+        type IN.
+        """
+        self.opi_gpio.remove_event_detect(pin)
+
+    def add_event_callback(self, pin, callback):
+        """Add a callback for an event already defined using add_event_detect().
+        Pin should be type IN.
+        """
+        self.opi_gpio.add_event_callback(pin, callback)
+
+    def event_detected(self, pin):
+        """Returns True if an edge has occured on a given GPIO.  You need to 
+        enable edge detection using add_event_detect() first.   Pin should be
+        type IN.
+        """
+        return self.opi_gpio.event_detected(pin)
+
+    def wait_for_edge(self, pin, edge):
+        """Wait for an edge.   Pin should be type IN.  Edge must be RISING,
+        FALLING or BOTH.
+        """
+        self.opi_gpio.wait_for_edge(pin, self._edge_mapping[edge])
+
+    def cleanup(self, pin=None):
+        """Clean up GPIO event detection for specific pin, or all pins if none 
+        is specified.
+        """
+        if pin is None:
+            self.opi_gpio.cleanup()
+        else:
+            self.opi_gpio.cleanup(pin)
+
 class AdafruitBBIOAdapter(BaseGPIO):
     """GPIO implementation for the Beaglebone Black using the Adafruit_BBIO
     library.
@@ -407,15 +509,18 @@ class AdafruitMinnowAdapter(BaseGPIO):
 
 def get_platform_gpio(**keywords):
     """Attempt to return a GPIO instance for the platform which the code is being
-    executed on.  Currently supports only the Raspberry Pi using the RPi.GPIO
-    library and Beaglebone Black using the Adafruit_BBIO library.  Will throw an
-    exception if a GPIO instance can't be created for the current platform.  The
-    returned GPIO object is an instance of BaseGPIO.
+    executed on.  Currently supports only the Orange Pi and the Raspberry Pi using 
+    the RPi.GPIO library and Beaglebone Black using the Adafruit_BBIO library.  
+    Will throw an exception if a GPIO instance can't be created for the current platform.  
+    The returned GPIO object is an instance of BaseGPIO.
     """
     plat = Platform.platform_detect()
     if plat == Platform.RASPBERRY_PI:
         import RPi.GPIO
         return RPiGPIOAdapter(RPi.GPIO, **keywords)
+    elif plat == Platform.ORANGE_PI:
+        import OPi.GPIO
+        return OPiGPIOAdapter(OPi.GPIO, **keywords)
     elif plat == Platform.BEAGLEBONE_BLACK:
         import Adafruit_BBIO.GPIO
         return AdafruitBBIOAdapter(Adafruit_BBIO.GPIO, **keywords)
